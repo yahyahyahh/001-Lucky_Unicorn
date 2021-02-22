9af4775b6e70a00d5dfe95c98354c280b4bb0c97
yahyahyahh <78515967+yahyahyahh@users.noreply.github.com>
2021-02-23 10:31:24 +1300

    02_y_n_checker

--- 01-yes_no_v1.py
+++ 01-yes_no_v1.py
@@ -1,3 +1,5 @@
+
+#Main Routine goes here...
 show_instructions = ""
 while show_instructions.lower() != "xxx":
 
@@ -6,10 +8,12 @@ while show_instructions.lower() != "xxx":
 
     # If they say yes, output 'program  continues'
     if show_instructions == "yes" or show_instructions == "y":
+        show_instructions = "yes"
         print("program continues")
 
     # If they say no, output 'display instructions'
     elif show_instructions == "no" or show_instructions == "n":
+        show_instructions = "no"
         print("display instructions")
 
     # If anything other than yes or no, output 'Please answer yes / no'
---
+++ y_n_checker.py
@@ -0,0 +1,22 @@
+
+
+# Function
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("Please answer yes / no")
+
+
+# Main Routine goes here...
+show_instructions = yes_no("Have you played the game before? ")
+print("You chose {}".format(show_instructions))
